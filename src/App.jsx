db04d15323a11776be271f55d6b057325c62e6e8
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2023-03-15 12:01:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Counter from "./features/counter/Counter";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
